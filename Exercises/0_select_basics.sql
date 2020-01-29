40a176a2a6e6cb3f50fe53635a02293a1232c039
Qoosim <ayinde.abdulghaniyy@gmail.com>
2020-01-29 17:22:56 +0100

    IN clause is used in a sql statement to check if an item is in a list

--- Exercises/0_select_basics.sql
+++ Exercises/0_select_basics.sql
@@ -3,3 +3,7 @@
 --1 => Modify it to show the population of Germany
 SELECT population FROM world
   WHERE name = 'Germany'
+
+--2 => Show the name and the population for 'Sweden', 'Norway' and 'Denmark'.
+SELECT name, population FROM world
+  WHERE name IN ('Sweden', 'Norway', 'Denmark');
