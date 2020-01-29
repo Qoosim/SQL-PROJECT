9629109594069ad9e7bc2d64c1a1ea78c6294966
Qoosim <ayinde.abdulghaniyy@gmail.com>
2020-01-29 22:45:28 +0100

    feat: where clause is used to filter records

--- Exercises/2_select_basics.sql
+++ Exercises/2_select_basics.sql
@@ -3,3 +3,8 @@
 --1 => SQL command to show the name, continent and population of all countries.
 SELECT name, continent, population
 FROM world
+
+-- 2 => Show the name for the countries that have a population of at least 200 million.
+SELECT name 
+FROM world
+WHERE population = 64105700
