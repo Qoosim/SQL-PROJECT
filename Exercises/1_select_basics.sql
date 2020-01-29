d55b743af499687cc14ee02d039f3697935162d0
Qoosim <ayinde.abdulghaniyy@gmail.com>
2020-01-29 21:46:42 +0100

    feat: WHERE capital LIKE concat ('%', name, '%') is used to find the countries that have their names  as part of their capital names

--- Exercises/1_select_basics.sql
+++ Exercises/1_select_basics.sql
@@ -59,3 +59,8 @@ WHERE name LIKE capital
 SELECT name
 FROM world
 WHERE capital LIKE concat(name, ' City')
+
+--13 => Find the capital and the name where the capital includes the name of the country.
+SELECT capital, name
+FROM world
+WHERE capital LIKE concat ('%', name, '%')
